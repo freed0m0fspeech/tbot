2ea511248f6b48d7cc2d10b932f8e088e32e9258
01eh <prostre1@ukr.net>
2022-03-30 19:53:45 +0300

    Main loop change

--- main.py
+++ main.py
@@ -122,6 +122,6 @@ if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
 
-    loop.run_until_complete(asyncio.sleep(0.250))
+    # loop.run_until_complete(asyncio.sleep(0.250))
     # loop.close()
     # main()
