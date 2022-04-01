11290330b4057da5f4669758da798e5eb2775693
01eh <prostre1@ukr.net>
2022-04-01 19:57:42 +0300

    April fools update

--- plugins/Bots/PyrogramBot/handlers.py
+++ plugins/Bots/PyrogramBot/handlers.py
@@ -1241,14 +1241,14 @@ class PyrogramBotHandler:
 
                 # print(message_id, chat_id, update)
 
-            while True:
-                try:
-                    if await self.pyrogramBot.user.send_reaction(chat_id=chat_id, message_id=message_id, emoji=random.choice(emoji)):
-                        return
-                except errors.FloodWait as e:
-                    await asyncio.sleep(e.x)
-                except Exception as e:
-                    return print(e)
+                while True:
+                    try:
+                        if await self.pyrogramBot.user.send_reaction(chat_id=chat_id, message_id=message_id, emoji=random.choice(emoji)):
+                            return
+                    except errors.FloodWait as e:
+                        await asyncio.sleep(e.x)
+                    except Exception as e:
+                        return print(e)
 
         # chat_member = await self.pyrogramBot.bot.get_chat_member(chat_id=chat_id, user_id='me')
 
