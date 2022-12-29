21c882eebba9d7ce978c4301baec25aabf08d2ce
01eh <prostre1@ukr.net>
2022-12-29 02:26:14 +0200

    Minor changes

--- main.py
+++ main.py
@@ -123,7 +123,3 @@ async def main():
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
-
-    # loop.run_until_complete(asyncio.sleep(0.250))
-    # loop.close()
-    # main()
