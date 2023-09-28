0ffae0821eb4647789ce7ebade50a18a1bc5f3c2
Oleh Zaporozhchenko <prostre1@ukr.net>
2023-09-28 22:18:46 +0300

    v1.9.0

--- jobs/jobs.py
+++ jobs/jobs.py
@@ -24,63 +24,61 @@ def stats_sync(query=None, filter=None):
 
     if not mongoDataBase.check_connection():
         return
-    try:
-        for chat_id in cache.stats.keys():
-            query = {'xp': 1}
-            filter = {'chat_id': chat_id}
-            document = mongoDataBase.get_document(database_name='tbot',
-                                                  collection_name='chats',
-                                                  filter=filter,
-                                                  query=query)
 
-            chat_xp = document.get('xp', {})
+    for chat_id in cache.stats.keys():
+        query = {'xp': 1}
+        filter = {'chat_id': chat_id}
+        document = mongoDataBase.get_document(database_name='tbot',
+                                              collection_name='chats',
+                                              filter=filter,
+                                              query=query)
 
-            cache.stats[chat_id]['xp']['message_xp'] = chat_xp.get('message_xp', 100)
-            cache.stats[chat_id]['xp']['voice_xp'] = chat_xp.get('voice_xp', 50)
-            cache.stats[chat_id]['xp']['message_xp_delay'] = chat_xp.get('message_xp_delay', 60)
+        chat_xp = document.get('xp', {})
 
-            query = {}
+        cache.stats[chat_id]['xp']['message_xp'] = chat_xp.get('message_xp', 100)
+        cache.stats[chat_id]['xp']['voice_xp'] = chat_xp.get('voice_xp', 50)
+        cache.stats[chat_id]['xp']['message_xp_delay'] = chat_xp.get('message_xp_delay', 60)
 
-            for user_id in cache.stats.get(chat_id, {}).get('members', {}).keys():
-                try:
-                    voicetime = cache.stats.get(chat_id, {}).get('members', {}).get(user_id, {}).pop('voicetime')
-                except Exception as e:
-                    voicetime = 0
+        query = {}
 
-                if not voicetime == 0:
-                    query[f'users.{user_id}.stats.voicetime'] = voicetime
+        for user_id in cache.stats.get(chat_id, {}).get('members', {}).keys():
+            try:
+                voicetime = cache.stats.get(chat_id, {}).get('members', {}).get(user_id, {}).pop('voicetime')
+            except Exception as e:
+                voicetime = 0
 
-                try:
-                    messages_count = cache.stats.get(chat_id, {}).get('members', {}).get(user_id, {}).pop(
-                        'messages_count')
-                except Exception as e:
-                    messages_count = 0
+            if not voicetime == 0:
+                query[f'users.{user_id}.stats.voicetime'] = voicetime
 
-                if not messages_count == 0:
-                    query[f'users.{user_id}.stats.messages_count'] = messages_count
+            try:
+                messages_count = cache.stats.get(chat_id, {}).get('members', {}).get(user_id, {}).pop(
+                    'messages_count')
+            except Exception as e:
+                messages_count = 0
 
-                try:
-                    messages_count_xp = cache.stats.get(chat_id, {}).get('members', {}).get(user_id, {}).pop(
-                        'messages_count_xp')
-                except Exception as e:
-                    messages_count_xp = 0
+            if not messages_count == 0:
+                query[f'users.{user_id}.stats.messages_count'] = messages_count
 
-                if not messages_count_xp == 0 or not voicetime == 0:
-                    message_xp = cache.stats.get(chat_id, {}).get('xp', {}).get('message_xp', 100)
-                    voice_xp = cache.stats.get(chat_id, {}).get('xp', {}).get('voice_xp', 50)
+            try:
+                messages_count_xp = cache.stats.get(chat_id, {}).get('members', {}).get(user_id, {}).pop(
+                    'messages_count_xp')
+            except Exception as e:
+                messages_count_xp = 0
 
-                    xp = (messages_count_xp * message_xp) + ((voicetime // 60) * voice_xp)
+            if not messages_count_xp == 0 or not voicetime == 0:
+                message_xp = cache.stats.get(chat_id, {}).get('xp', {}).get('message_xp', 100)
+                voice_xp = cache.stats.get(chat_id, {}).get('xp', {}).get('voice_xp', 50)
 
-                    query[f'users.{user_id}.stats.xp'] = xp
+                xp = (messages_count_xp * message_xp) + ((voicetime // 60) * voice_xp)
 
-                # del cache.stats[guild_id]['members'][member_id]['messages_count']
+                query[f'users.{user_id}.stats.xp'] = xp
 
-            mongoUpdate = mongoDataBase.update_field(database_name='tbot', collection_name='chats',
-                                                          action='$inc', filter=filter, query=query)
+            # del cache.stats[guild_id]['members'][member_id]['messages_count']
 
-            if mongoUpdate is None:
-                date = datetime.now(tz=utc) + timedelta(minutes=15)
-                date = date.strftime('%Y-%m-%d %H:%M:%S')
-                sched.get_job('stats_sync').modify(next_run_time=date, args=[query, filter])
-    except Exception as e:
-        print(e)
+        mongoUpdate = mongoDataBase.update_field(database_name='tbot', collection_name='chats',
+                                                      action='$inc', filter=filter, query=query)
+
+        if mongoUpdate is None:
+            date = datetime.now(tz=utc) + timedelta(minutes=15)
+            date = date.strftime('%Y-%m-%d %H:%M:%S')
+            sched.get_job('stats_sync').modify(next_run_time=date, args=[query, filter])
